e084bc20294b7c912b3d8c20e62de59af79033f0
Ben VandenBos <bvandenbos@gmail.com>
2011-05-12 08:54:55 -0700

    Fixing syntax error in Meme

--- lib/robut/plugin/meme.rb
+++ lib/robut/plugin/meme.rb
@@ -18,7 +18,7 @@ class Robut::Plugin::Meme < Robut::Plugin::Base
   # more than one line of text, lines should be separated with a semicolon.
   def handle(time, nick, message)
     word = words(message).first
-    if sent_to_me?(message && Meme::GENERATORS.has_key?(word.upcase)
+    if sent_to_me?(message && Meme::GENERATORS.has_key?(word.upcase))
       words = words(message)
       g = Meme.new(words.shift.upcase)
       line1, line2 = words.join(' ').split(';')
