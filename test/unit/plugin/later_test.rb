ca46f8b49d141090cae141067c224f979f6a51e4
Michael Lavrisha <michael.lavrisha@gmail.com>
2011-10-29 16:56:56 -0600

    Passing LaterTest

--- test/unit/plugin/later_test.rb
+++ test/unit/plugin/later_test.rb
@@ -5,7 +5,8 @@ class Robut::Plugin::LaterTest < Test::Unit::TestCase
 
   def setup
     @connection = Robut::ConnectionMock.new
-    @plugin = Robut::Plugin::Later.new(@connection)
+    @presence = Robut::PresenceMock.new(@connection)
+    @plugin = Robut::Plugin::Later.new(@presence)
     @plugin.instance_eval do
       def threader; yield; end # no threads
       def sleep(*prms); end # just skip the whole sleeping part
@@ -14,8 +15,8 @@ class Robut::Plugin::LaterTest < Test::Unit::TestCase
 
   def test_replies_with_minutes
     @plugin.handle(Time.now, "@john", "@robut in 0 minutes msg me")
-    assert_equal ["Ok, see you in 0 minutes"], @plugin.connection.replies
-    message = @plugin.connection.messages.first
+    assert_equal ["Ok, see you in 0 minutes"], @plugin.reply_to.replies
+    message = @plugin.reply_to.messages.first
     assert message
     assert_equal message[1], "@john"
     assert_equal message[2], "@robut msg me"
@@ -23,17 +24,17 @@ class Robut::Plugin::LaterTest < Test::Unit::TestCase
 
   def test_replies_with_sec
     @plugin.handle(Time.now, "@john", "@robut in 1 sec msg me")
-    assert_equal ["Ok, see you in 1 sec"], @plugin.connection.replies
+    assert_equal ["Ok, see you in 1 sec"], @plugin.reply_to.replies
   end
   
   def test_replies_with_hr
     @plugin.handle(Time.now, "@john", "@robut in 1 hr msg me")
-    assert_equal ["Ok, see you in 1 hr"], @plugin.connection.replies
+    assert_equal ["Ok, see you in 1 hr"], @plugin.reply_to.replies
   end
 
   def test_replies_with_hrs
     @plugin.handle(Time.now, "@john", "@robut in 2 hrs msg me")
-    assert_equal ["Ok, see you in 2 hrs"], @plugin.connection.replies
+    assert_equal ["Ok, see you in 2 hrs"], @plugin.reply_to.replies
   end
   
 end
