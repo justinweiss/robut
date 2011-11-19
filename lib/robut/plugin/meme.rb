f584ae54f45c2ea65ea4017ccefc5cd48dd77b5d
Ben VandenBos <bvandenbos@gmail.com>
2011-11-19 01:19:10 -0800

    Updating meme plugin to take any url or memecaptain reference

--- lib/robut/plugin/meme.rb
+++ lib/robut/plugin/meme.rb
@@ -4,27 +4,11 @@ require 'cgi'
 class Robut::Plugin::Meme
   include Robut::Plugin
 
-  MEMES = {
-    'bear_grylls' => 'http://memecaptain.com/bear_grylls.jpg',
-    'insanity_wolf' => 'http://memecaptain.com/insanity_wolf.jpg',
-    'most_interesting' => 'http://memecaptain.com/most_interesting.jpg',
-    'philosoraptor' => 'http://memecaptain.com/philosoraptor.jpg',
-    'scumbag_steve' => 'http://memecaptain.com/scumbag_steve.jpg',
-    'town_crier' => 'http://memecaptain.com/town_crier.jpg',
-    'troll_face' => 'http://memecaptain.com/troll_face.jpg',
-    'y_u_no' => 'http://memecaptain.com/y_u_no.jpg',
-    'yao_ming' => 'http://memecaptain.com/yao_ming.jpg',
-    'business_cat' => 'http://memecaptain.com/business_cat.jpg',
-    'all_the_things' => 'http://memecaptain.com/all_the_things.jpg',
-    'fry' => 'http://memecaptain.com/fry.png',
-    'sap' => 'http://memecaptain.com/sap.jpg'
-  }
-
   # Returns a description of how to use this plugin
   def usage
     [
-      "#{at_nick} meme list - lists all the memes that #{nick} knows about",
-      "#{at_nick} meme <meme> <line1>;<line2> - responds with a link to a generated <meme> image using <line1> and <line2>"
+      "#{at_nick} meme <meme> <line1>;<line2> - responds with a link to a generated <meme> image using <line1> and <line2>.  " +
+      "See http://memecaptain.com/ for a list of memes.  You can also pass a link to your own image as the meme."
     ]
   end
 
@@ -47,17 +31,15 @@ class Robut::Plugin::Meme
     return unless command == 'meme'
     meme = words.shift
 
-    if meme == 'list'
-      reply("Memes available: #{MEMES.keys.join(', ')}")
-    elsif MEMES[meme]
-      url = CGI.escape(MEMES[meme])
-      line1, line2 = words.join(' ').split(';').map { |line| CGI.escape(line.strip)}
-      meme_url = "http://memecaptain.com/i?u=#{url}&tt=#{line1}"
-      meme_url += "&tb=#{line2}" if line2
-      reply(meme_url)
+    if meme.include?("://")
+      url = meme
     else
-      reply("Meme not found: #{meme}")
+      url = "http://memecaptain.com/#{meme}.jpg"
     end
+    line1, line2 = words.join(' ').split(';').map { |line| CGI.escape(line.strip)}
+    meme_url = "http://memecaptain.com/i?u=#{url}&tt=#{line1}"
+    meme_url += "&tb=#{line2}" if line2
+    reply(meme_url)    
   end
 
 end
