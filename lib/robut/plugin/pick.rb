8268f0959fdac1e9cf7d810a3f53ee261bd69c61
Ben VandenBos <bvandenbos@gmail.com>
2012-01-10 12:00:53 -0800

    re-writing pick plugin to use the new hawtness

--- lib/robut/plugin/pick.rb
+++ lib/robut/plugin/pick.rb
@@ -4,21 +4,13 @@ require 'calc'
 class Robut::Plugin::Pick
   include Robut::Plugin
 
-  # Returns a description of how to use this plugin
-  def usage
-    "#{at_nick} pick <a>, <b>, <c>, ...  - randomly selects one of the options"
+  desc "pick <a>, <b>, <c>, ...  - randomly selects one of the options"
+  match /^pick (.*)/, :sent_to_me => true do |message|
+    options = message.split(',').map { |s| s.strip }
+    rsp = options[random(options.length)]
+    reply("And the winner is... #{rsp}") if rsp
   end
-  
-  # Perform the calculation specified in +message+, and send the
-  # result back.
-  def handle(time, sender_nick, message)
-    if sent_to_me?(message) && words(message).first == 'pick'
-      options = words(message, 'pick').join(' ').split(',').map { |s| s.strip }
-      rsp = options[random(options.length)]
-      reply("And the winner is... #{rsp}") if rsp
-    end
-  end
-  
+
   def random(c)
     rand(c)
   end
