05463aca1823edbf9bc654ef8e5328ef8535c529
Justin Weiss <justin@justinweiss.com>
2016-10-14 13:09:45 -0700

    First attempt at responding to invitations.
    Still needs tests and some cleanup.

--- lib/robut/pm.rb
+++ lib/robut/pm.rb
@@ -1,12 +1,40 @@
 class Robut::PM < Robut::Presence
 
-  def initialize(connection, rooms)
+  def initialize(connection)
     # Add the callback from direct messages. Turns out the
     # on_private_message callback doesn't do what it sounds like, so I
     # have to go a little deeper into xmpp4r to get this working.
     self.connection = connection
-    connection.client.add_message_callback(200, self) do |message|
-      from_room = rooms.any? {|room| room.muc.from_room?(message.from)}
+    add_invite_callback(100)
+    add_message_callback(200)
+  end
+
+  def reply(message, to)
+    unless to.kind_of?(Jabber::JID)
+      to = find_jid_by_name(to)
+    end
+
+    msg = Jabber::Message.new(to, message)
+    msg.type = :chat
+    connection.client.send(msg)
+  end
+
+  private
+
+  def is_invite?(message)
+    puts message.to_s
+    message.type.nil? && message.x.elements["//invite"]
+  end
+
+  def add_invite_callback(priority)
+    connection.client.add_message_callback(100, self) do |message|
+      connection.join_room(message.from.node) if is_invite?(message)
+    end
+  end
+
+  def add_message_callback(priority)
+    connection.client.add_message_callback(priority, self) do |message|
+      from_room = connection.rooms.any? {|room| room.muc.from_room?(message.from)}
       if !from_room && message.type == :chat && message.body
         time = Time.now # TODO: get real timestamp? Doesn't seem like
                         # jabber gives it to us
@@ -20,13 +48,9 @@ class Robut::PM < Robut::Presence
     end
   end
 
-  def reply(message, to)
-    unless to.kind_of?(Jabber::JID)
-      to = find_jid_by_name(to)
-    end
-
-    msg = Jabber::Message.new(to, message)
-    msg.type = :chat
-    connection.client.send(msg)
+  # Find a jid in the roster with the given name, case-insensitively
+  def find_jid_by_name(name)
+    name = name.downcase
+    connection.roster.items.detect {|jid, item| item.iname.downcase == name}.first
   end
 end
